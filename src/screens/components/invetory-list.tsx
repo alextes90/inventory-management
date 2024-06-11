f089a53daa00c6358aa02a52bc1ece6fecc36219
Alex Teslin <a.teslin90@gmail.com>
2024-06-11 18:15:54 +0300

    feat: add inventory-list component

---
+++ src/screens/components/invetory-list.tsx
@@ -0,0 +1,34 @@
+import { InventoryItem } from "@/models";
+
+type Props = {
+  inventoryItemList: InventoryItem[];
+  setChangeItemName: (item: string) => void;
+  setChangeItemQuant: (item: number) => void;
+};
+export const InventoryList = ({
+  inventoryItemList,
+  setChangeItemName,
+  setChangeItemQuant,
+}: Props) => {
+  return (
+    <>
+      <div className='flex justify-between min-w-[500px] font-bold'>
+        <p>Name</p>
+        <p>Quantity</p>
+      </div>
+      {inventoryItemList.map((el) => (
+        <div
+          key={el.name}
+          className='flex justify-between min-w-[300px] cursor-pointer hover:bg-sky-100 p-[5px]'
+          onClick={() => {
+            setChangeItemName(el.name);
+            setChangeItemQuant(el.quantity);
+          }}
+        >
+          <p>{el.name}</p>
+          <p className='pr-[5px]'>{el.quantity}</p>
+        </div>
+      ))}
+    </>
+  );
+};
