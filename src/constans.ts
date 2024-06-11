cb005408f5698d54fc864b8f8fc3306597f9cfd7
Alex Teslin <a.teslin90@gmail.com>
2024-06-11 18:14:11 +0300

    feat: add app const

---
+++ src/constans.ts
@@ -0,0 +1,5 @@
+export const INVENTORY_PATH = "inventory";
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const GET_ALL_PRODUCT_PATH = "product/all";
+export const PRODUCT_PAGE = "/product";
+export const HOME_PAGE = "/";
