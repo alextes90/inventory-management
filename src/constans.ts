a9e046c6abb1eba120943cdfb2b88e74a4f00836
Alex Teslin <a.teslin90@gmail.com>
2024-06-12 12:19:01 +0300

    feat: add pathes

--- src/constans.ts
+++ src/constans.ts
@@ -1,5 +1,8 @@
 export const INVENTORY_PATH = "inventory";
+export const PRODUCT_PATH = "product";
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const GET_ALL_PRODUCT_PATH = "product/all";
 export const PRODUCT_PAGE = "/product";
 export const HOME_PAGE = "/";
+export const DEFAULT_PRODUCT_NAME = "choose product";
+export const RESET_INVENTIRY_PATH = "inventory/reset";
